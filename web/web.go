3999000ac117831d3b6495581d8c4db267363ea0
Yiqun Hu <yiqun.hu@gmail.com>
2015-03-29 20:55:58 -0400

    Add Archive Function

--- web/web.go
+++ web/web.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 	"time"
+	"path/filepath"
 	"net/http"
+	"html/template"
 	"io/ioutil"
 )
+
+type Archive struct {
+	Name string
+	FileName string
+}
 
 /*
 func main() {
@@ -23,6 +30,8 @@ func main() {
 
 
 	http.HandleFunc("/data/", hello)
+	http.HandleFunc("/archives/", archives)
+	http.HandleFunc("/archive", viewarchive)
 	fmt.Println("listening...")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
@@ -40,4 +49,32 @@ func hello(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "application/json")
 	res.Header().Set("Access-Control-Allow-Origin", "*")
 	fmt.Fprintln(res, string(data))
+}
+
+func viewarchive(res http.ResponseWriter, req *http.Request) {
+	filename := "../data/" + req.URL.Query().Get("filename")
+        data, _ := ioutil.ReadFile(filename)
+
+        res.Header().Set("Content-Type", "application/json")
+        res.Header().Set("Access-Control-Allow-Origin", "*")
+        fmt.Fprintln(res, string(data))
+}
+
+
+func archives(res http.ResponseWriter, req *http.Request) {
+	dirname := "../data/"
+	files, _ := ioutil.ReadDir(dirname)
+
+	archives := []Archive{}	
+
+	for _,file := range files {
+		ext := filepath.Ext(file.Name())
+		name := file.Name()
+		archives = append(archives, Archive{name[8:len(name)-len(ext)], name})
+	}
+
+	fmt.Println(len(files))
+
+	t, _ := template.ParseFiles("../static/archives.html")
+	t.Execute(res, archives)	
 }
